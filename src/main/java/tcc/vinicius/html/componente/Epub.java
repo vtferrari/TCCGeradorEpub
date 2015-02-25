47ae09680a5214a34f3efec997f37eb20a21fafe
vinicius <vinicius@vinicius-NOTE>
2015-02-24 21:53:05 -0300

    Criando Conteudo

---
+++ src/main/java/tcc/vinicius/html/componente/Epub.java
@@ -0,0 +1,74 @@
+package tcc.vinicius.html.componente;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import nl.siegmann.epublib.domain.Author;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Metadata;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubWriter;
+import nl.siegmann.epublib.service.MediatypeService;
+import tcc.vinicius.mvn.model.Capitulo;
+
+public class Epub {
+
+    private final EpubWriter epubWriter;
+    private final Metadata metadata;
+    private final Book book;
+    private List<Capitulo> tabelaDeConteudo;
+    private final String nomeArquivo;
+    private static final String PRE_HTML = "<html><header><title>#{Titulo}</title><header/><body>";
+    private static final String POS_HTML = "</body></html>";
+
+    public Epub(String nomeArquivo) {
+        this.book = new Book();
+        this.metadata = book.getMetadata();
+        this.epubWriter = new EpubWriter();
+        this.nomeArquivo = nomeArquivo;
+        this.tabelaDeConteudo = new ArrayList<>();
+    }
+
+    public void processarConteudo() throws FileNotFoundException, IOException {
+        for (Capitulo toc : tabelaDeConteudo) {
+            String conteudoHTML = PRE_HTML.replace("#{Titulo}", toc.getTitulo()) + toc.getConteudo() + POS_HTML;
+            this.criarConteudo(toc.getTitulo(), conteudoHTML);
+        }
+        epubWriter.write(book, new FileOutputStream("C:\\temp\\" + nomeArquivo + ".epub"));
+    }
+
+    public void addTitulo(String titulo) {
+        metadata.addTitle(titulo);
+    }
+
+    public void addAutor(String nome, String sobrenome) {
+        metadata.addAuthor(new Author(nome, sobrenome));
+    }
+
+    public void addEditora(String editora) {
+        metadata.addPublisher(editora);
+    }
+
+    public Metadata metaDados() {
+        return metadata;
+    }
+
+    public void addCapitulos(List<Capitulo> tabelaDeConteudo) {
+        this.tabelaDeConteudo = tabelaDeConteudo;
+    }
+
+    public void criarConteudo(String titulo, String conteudo) throws IOException {
+        Resource resource = new Resource("id" + conteudo.hashCode(),
+                conteudo.getBytes(), titulo + ".html", MediatypeService.XHTML);
+        resource.close();
+        book.getTableOfContents().addSection(resource, titulo);
+    }
+
+    public Book getEBook() {
+        return book;
+    }
+
+}
---
+++ src/test/java/tcc/vinicius/html/componente/EpubTest.java
@@ -0,0 +1,83 @@
+package tcc.vinicius.html.componente;
+
+import java.io.File;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import tcc.vinicius.mvn.model.Capitulo;
+
+public class EpubTest {
+
+    public EpubTest() {
+    }
+
+    @After
+    public void depois() {
+        File file = new File("C:\\temp\\epub.epub");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    @Test
+    public void testDeveCriarUmEpubVazio() throws Exception {
+        Epub epub = new Epub("epub");
+        epub.addCapitulos(new ArrayList<Capitulo>());
+        epub.processarConteudo();
+        assertTrue(new File("C:\\temp\\epub.epub").exists());
+    }
+
+    @Test
+    public void testDeveCriarEpubComMetaInformacoes() throws Exception {
+        Epub epub = new Epub("nome");
+        epub.addTitulo("titulo");
+        epub.addAutor("nome", "sobrenome");
+        epub.addEditora("editora");
+        assertEquals("titulo", epub.metaDados().getTitles().get(0));
+        assertEquals("nome", epub.metaDados().getAuthors().get(0).getFirstname());
+        assertEquals("sobrenome", epub.metaDados().getAuthors().get(0).getLastname());
+        assertEquals("editora", epub.metaDados().getPublishers().get(0));
+    }
+
+    @Test
+    public void testDeveCriarEbookComConteuDoEseusMetas() throws Exception {
+        Epub epub = new Epub("nome");
+        epub.criarConteudo("titulo", "<p>conteudo</p>");
+        assertFalse(epub.getEBook().getTableOfContents().size() < 1);
+        assertEquals(15, epub.getEBook().getTableOfContents().getAllUniqueResources().get(0).getSize());
+    }
+
+    @Test
+    public void testDeveCriarEbookComConteuDoEseusMetasEUmArquivo() throws Exception {
+        Epub epub = new Epub("livro");
+        epub.criarConteudo("titulo", "<p>conteudo</p>");
+        assertFalse(epub.getEBook().getTableOfContents().size() < 1);
+        assertEquals(15, epub.getEBook().getTableOfContents().getAllUniqueResources().get(0).getSize());
+        epub.processarConteudo();
+        assertTrue(new File("C:\\temp\\livro.epub").exists());
+    }
+    @Test
+    public void testDeveCriarEbookComListaDeConteuDoEseusMetasEUmArquivo() throws Exception {
+        ArrayList<Capitulo> toc = new ArrayList<>();
+        Capitulo c1 = new Capitulo();
+        c1.setTitulo("1");
+        c1.setConteudo("<h1>1</h1>");
+        Capitulo c2 = new Capitulo();
+        c2.setTitulo("2");
+        c2.setConteudo("2");
+        toc.add(c1);
+        toc.add(c2);
+        Epub epub = new Epub("nome");
+        epub.addTitulo("titulo");
+        epub.addAutor("nome", "sobrenome");
+        epub.addEditora("editora");
+        epub.addCapitulos(toc);
+        epub.processarConteudo();
+        assertEquals(2,epub.getEBook().getTableOfContents().size());
+        String str = new String(epub.getEBook().getTableOfContents().getTocReferences().get(0).getResource().getData(), "UTF-8");
+        assertEquals("<html><header><title>1</title><header/><body><h1>1</h1></body></html>",str);
+        assertTrue(new File("C:\\temp\\nome.epub").exists());
+    }
+
+}
